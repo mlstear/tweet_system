868a46da9ab1d07ba21237a6f221db39f0744b75
reo.murata <lavi.mlty@gmail.com>
2020-04-07 19:23:31 +0900

    Add Tweet index

---
+++ WebContent/WEB-INF/views/topPage/topIndex.jsp
@@ -0,0 +1,45 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<c:import url="../layout/app.jsp">
+  <c:param name="content">
+     <c:if test="${flush!=null}">
+        <div id="flush_success">
+           <c:out value="${flush}"></c:out>
+        </div>
+     </c:if>
+     <h2>最新のtweet</h2>
+     <p><a href="<c:out value='/tweets/new'/>">新規　投稿する</a></p>
+     <table id="tweet_list">
+        <tbody>
+          <c:forEach var="tweet" items="${tweets}" varStatus="status">
+            <tr class="row${status.count%2}">
+               <td class="tweet_name">
+                  <c:out value="${tweet.poster.name}"/>&nbsp;&nbsp;
+                  <!-- ここにフォローシステムいれる -->
+               </td>
+               <td class="tweet_created_at"><fmt:formatDate value="${tweet.created_at}" pattern="yyyy-MM-dd HH:mm"/></td>
+               <td class="tweet_content">
+                  <pre><c:out value="${tweet.content}" /></pre>
+               </td>
+            </tr>
+          </c:forEach>
+        </tbody>
+
+     </table>
+
+     <div id="pagination">
+        （全${tweets_count}件）<br/>
+         <c:forEach var="i" begin="1" end="${((tweets_count-1)/30)+1}" step="1">
+            <c:choose>
+               <c:when test="${i==page}">
+                  <c:out value="${i}" />&nbsp;
+               </c:when>
+               <c:otherwise>
+                 <a href="<c:url value='/index.html?page=${i}' />"><c:out value="${i}" /></a>&nbsp;
+               </c:otherwise>
+            </c:choose>
+         </c:forEach>
+     </div>
+  </c:param>
+</c:import>
--- src/controllers/toppage/TopPageIndexServlet.java
+++ src/controllers/toppage/TopPageIndexServlet.java
@@ -1,13 +1,18 @@
 package controllers.toppage;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.persistence.EntityManager;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Tweet;
+import utils.DBUtil;
 
 /**
  * Servlet implementation class TopPageIndexServlet
@@ -28,12 +33,36 @@ public class TopPageIndexServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+
+        int page;
+        try{
+            page=Integer.parseInt(request.getParameter("page"));
+        }catch(Exception e){
+            page=1;
+        }
+        List<Tweet> tweets=em.createNamedQuery("getAllTweets",Tweet.class)
+                             .setFirstResult(30*(page-1))
+                             .setMaxResults(30)
+                             .getResultList();
+
+        long tweets_count=(long)em.createNamedQuery("getTweetsCount",Long.class)
+                                  .getSingleResult();
+
+        em.close();
+
+        request.setAttribute("tweets", tweets);
+        request.setAttribute("tweets_count", tweets_count);
+        request.setAttribute("page", page);
+
+
+
         if(request.getSession().getAttribute("flush")!=null){
             request.setAttribute("flush", request.getSession().getAttribute("flush"));
             request.getSession().removeAttribute("flush");
         }
 
-        RequestDispatcher rd=request.getRequestDispatcher("/WEB-INF/views/topPage/index.jsp");
+        RequestDispatcher rd=request.getRequestDispatcher("/WEB-INF/views/topPage/topIndex.jsp");
         rd.forward(request, response);
     }
 
